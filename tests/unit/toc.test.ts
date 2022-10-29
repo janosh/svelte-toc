63390400093e24c97950209b742591875112b4b5
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-10-29 12:39:36 -0700

    add test 'ToC lists expected headings'
    fix netlify build error due to missing pkg devalue

--- tests/unit/toc.test.ts
+++ tests/unit/toc.test.ts
@@ -5,6 +5,10 @@ beforeEach(() => {
   document.body.innerHTML = ``
 })
 
+function sleep(ms: number = 1) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 describe(`Toc`, () => {
   test(`renders custom title`, async () => {
     const toc = new Toc({
@@ -29,4 +33,39 @@ describe(`Toc`, () => {
       `Another custom title`
     )
   })
+
+  test.each([
+    [null, 3, [...Array(3).keys()].map((idx) => `Heading ${idx + 2}`)],
+    [
+      `body > :is(h1, h2, h3, h4, h5, h6)`,
+      6,
+      [...Array(6).keys()].map((idx) => `Heading ${idx + 1}`),
+    ],
+    [`h1:not(.toc-exclude)`, 0, []],
+  ])(
+    `ToC lists expected headings`,
+    async (headingSelector, expected_lis, expected_text) => {
+      document.body.innerHTML = `
+      <h1 class="toc-exclude">Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+    `
+      let props = {}
+      if (headingSelector) props = { headingSelector }
+
+      const toc = new Toc({
+        target: document.body,
+        props,
+      })
+      expect(toc).toBeTruthy()
+      await sleep()
+
+      const toc_ul = document.querySelector(`aside.toc ul`)
+      expect(toc_ul?.children.length).toBe(expected_lis)
+      expect(toc_ul?.textContent?.trim()).toBe(expected_text.join(` `))
+    }
+  )
 })
