94321688ec4e324a54ee25cc7690ffe5d4090eaf
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-03 16:45:51 -0800

    overhaul error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,45 +1,48 @@
 <script lang="ts">
-  import { dev } from '$app/environment'
   import { page } from '$app/stores'
+  import { homepage, name } from '../../package.json'
+
+  let online: boolean
 </script>
 
 <svelte:head>
-  <title>{$page.status}</title>
+  <title>Error {$page.status} &bull; {name}</title>
 </svelte:head>
 
-<div>
-  <h1>{$page.error?.name} {$page.status}</h1>
+<svelte:window bind:online />
 
-  {#if $page.status === 404}
+<div>
+  <h1>Error {String($page.status).replace(`0`, `😵`)}: {$page.error?.message}</h1>
+  {#if $page.status >= 500}
     <p>
-      Page not found. Back to <a href="/">landing page</a>. 🤦
+      If page reloading doesn't help, please raise an issue on
+      <a href="{homepage}/issues" target="_blank" rel="noreferrer">GitHub</a>. Thanks! 🙏
     </p>
   {/if}
-
-  {#if dev && $page.error?.stack}
-    <h2>Stack Trace</h2>
-    <pre>{$page.error.stack}</pre>
+  {#if online === false}
+    Looks like you're offline. If you think your connection is fine, check the
+    <a href="https://githubstatus.com">GitHub status page</a>
+    as this site is hosted by GitHub Pages.
   {/if}
+
+  <p>
+    Back to <a href=".">
+      <img src="favicon.svg" alt={name} height="30" />
+      landing page
+    </a>.
+  </p>
 </div>
 
 <style>
-  h1 {
-    text-align: center;
-  }
   div {
+    font-size: 1.2em;
     max-width: 45em;
-    padding: 5em 3em;
+    padding: 5em 3em 1em;
     margin: auto;
-  }
-  p {
     text-align: center;
   }
-  pre {
-    overflow: scroll;
-    font-size: 0.9em;
-    white-space: pre-wrap;
-    background: var(--accentBg);
-    padding: 5pt 1em;
-    border-radius: 3pt;
+  p img {
+    vertical-align: middle;
+    margin: 0 1pt 0 3pt;
   }
 </style>
