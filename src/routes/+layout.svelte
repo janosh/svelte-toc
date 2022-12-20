b2dc5e86ad67576f3f6fe6eb969804994f9e33f6
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-20 08:42:17 -0600

    add contributing.md
    split release step in test.yml into its own job

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import GitHubCorner from 'svelte-github-corner'
   import '../app.css'
 </script>
 
 <GitHubCorner href="https://github.com/janosh/svelte-toc" />
 
+{#if !$page.error && $page.url.pathname !== `/`}
+  <a href="." aria-label="Back to index page">&laquo; home</a>
+{/if}
+
 <slot />
+
+<style>
+  a[href='.'] {
+    font-size: 15pt;
+    position: absolute;
+    top: 2em;
+    left: 2em;
+    background-color: rgba(255, 255, 255, 0.1);
+    padding: 1pt 5pt;
+    border-radius: 3pt;
+    transition: 0.2s;
+  }
+  a[href='.']:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+</style>
---
+++ src/routes/contributing/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Toc from '$lib'
+  import Contributing from '../../../contributing.md'
+</script>
+
+<main>
+  <Contributing />
+</main>
+
+<Toc headingSelector="main > :where(h2)" breakpoint={1250} />
+
+<style>
+  :global(aside.toc.desktop) {
+    position: fixed;
+    top: 3em;
+    left: calc(50vw + 55em / 2);
+    max-width: 16em;
+  }
+</style>
