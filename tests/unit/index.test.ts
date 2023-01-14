84c1854f037c214e5d736982b2d834c8f278bf0f
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-14 15:36:41 -0800

    tweak readme prop docs
    update deps

--- tests/unit/index.test.ts
+++ tests/unit/index.test.ts
@@ -1,7 +1,8 @@
-import IndexToc from '$lib'
+import IndexToc, { Toc as NamedToc } from '$lib'
 import Toc from '$lib/Toc.svelte'
 import { expect, test } from 'vitest'
 
-test(`src/lib/index.ts default export is Toc component`, () => {
+test(`src/lib/index.ts has default and named export of Toc component`, () => {
   expect(IndexToc).toBe(Toc)
+  expect(NamedToc).toBe(Toc)
 })
